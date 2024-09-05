d1542411e76d2158e4e9ebdad715aa3dfc695a64
Cesar.rs.5 <Cesar.rs.5@hotmail.com>
2024-09-05 16:09:02 -0600

    Parcial01 - Add DataContext

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,8 @@
+namespace API.Entities;
+
+public class AppsUser
+{ 
+    public int Id {get; set;}
+
+    public required string UserName {get; set;}
+}
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e903106d483c8b8668decdc5bcf3014cc693de2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a95c168478928818cb6e9178f27b169e1831a778")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
