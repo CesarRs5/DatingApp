e903106d483c8b8668decdc5bcf3014cc693de2f
Cesar.rs.5 <Cesar.rs.5@hotmail.com>
2024-09-05 10:19:03 -0600

    Parcial01 - Second Commit

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19bcfeb59607238a0297a214682b3f5eb70a2bd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d39019279595f421207af718acb00b56bcb0218")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
