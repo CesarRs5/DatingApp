20c8100122a2cfa82cbe6764212c12cc4251b913
César Reynoso <128648447+CesarRs5@users.noreply.github.com>
2024-10-04 18:40:09 -0600

    Parcial01 - PARENT to CHILD communication

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {  } from '@angular/common';
-import { HttpClient, } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
@@ -14,14 +13,10 @@ import { HomeComponent } from "./home/home.component";
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  
   title = 'Date me';
-  http = inject(HttpClient);
-  users : any;
   private  accountService   =inject (AccountService);
   
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -32,12 +27,6 @@ export class AppComponent implements OnInit {
     this.accountService.currentUser.set(user);
   }
 
-  getUsers() {
-    this.http.get("https://localhost:5001/api/users").subscribe({
-      next: (response) => { this.users = response; },
-      error: (error) => { console.log(error); },
-      complete: () => { console.log("Request completed!"); }
-    });
-  }
+ 
  
 }
