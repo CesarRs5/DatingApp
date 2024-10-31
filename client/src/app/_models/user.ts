78702668d20ac00d045efbcc6a1d796ecbe72b93
cesar.rs.5 <cesar.rs.5@gmail.com>
2024-10-31 17:54:28 -0600

    Parcial03 - Add members support

--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -1,4 +1,5 @@
 export interface User {
     username: string;
-    toke: string;
+    token: string;
 }
+
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RegisterComponent } from "../register/register.component";
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-home',
@@ -10,14 +9,8 @@ import { Observable } from 'rxjs';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  {
   registerMode =false;
-  http = inject (HttpClient);
-  users:any;
-
-  ngOnInit(): void {
-    this.getUsers();
-  }
 
   registerToggle():void{
     this.registerMode=!this.registerMode;
@@ -29,11 +22,5 @@ export class HomeComponent implements OnInit {
     //console.log(this.registerMode);
   }
 
-  getUsers() {
-    this.http.get("https://localhost:5001/api/users").subscribe({
-      next: (response) => { this.users = response; },
-      error: (error) => { console.log(error); },
-      complete: () => { console.log("Request completed!"); }
-    });
-  }
+  
 }
