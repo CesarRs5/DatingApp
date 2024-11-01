28a2912a1700192bde51c2de7d18ac4e980b433e
cesar.rs.5 <cesar.rs.5@gmail.com>
2024-11-01 00:03:28 -0600

    Parcial03 - Add member card

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f35d3a2c441858292279c57ec99b3a64dc1c3109")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78702668d20ac00d045efbcc6a1d796ecbe72b93")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
