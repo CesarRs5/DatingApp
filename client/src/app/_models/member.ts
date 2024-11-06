b7f330bb7f80c2f719aec69f0a4ec35e727df551
cesar.rs.5 <cesar.rs.5@gmail.com>
2024-11-05 23:33:30 -0600

    Parcial03 - Add routing to member detail page

--- client/src/app/_models/member.ts
+++ client/src/app/_models/member.ts
@@ -3,7 +3,7 @@ import { Photo } from "./photo";
 
 export interface Member {
     id: number;
-    username: string;
+    userName: string;
     age: number;
     photoUrl: string;
     knownAs: string;
