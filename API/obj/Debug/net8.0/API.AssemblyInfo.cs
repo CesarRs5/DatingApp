6429c77b34e58a269fe0f9bee71a7a1fc438f1ba
cesar.rs.5 <cesar.rs.5@gmail.com>
2024-12-03 22:22:37 -0600

    Parcial03 - Add member edit component

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f773e45d22fb190efff28150cc69afc1d337e26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c84b9097f02497b6218d61bf52ff4aa8705b3d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
